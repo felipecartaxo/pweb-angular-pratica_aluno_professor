d6fee3ddb12234ef7dfa83f14f9c185e465f0c5c
Neraky <felipecartaxo1@gmail.com>
2025-01-25 18:22:33 -0300

    clonando repositório da atividade

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import {Aluno} from "./shared/modelo/aluno";
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  standalone: false,
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+
+
+}
