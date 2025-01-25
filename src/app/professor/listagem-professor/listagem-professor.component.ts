524b7c145040c6282fc6d6509ddebec0063feb8f
Neraky <felipecartaxo1@gmail.com>
2025-01-25 19:31:44 -0300

    adicionando componente de listagem de professores

--- src/app/professor/listagem-professor/listagem-professor.component.ts
+++ src/app/professor/listagem-professor/listagem-professor.component.ts
@@ -7,6 +7,8 @@ import { Component } from '@angular/core';
   templateUrl: './listagem-professor.component.html',
   styleUrl: './listagem-professor.component.css'
 })
+
+// Nome do módulo de listagem de professores que deverá ser colocado no arquivo de rotas
 export class ListagemProfessorComponent {
 
 }
