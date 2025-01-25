97679e1e60c3b6fa3d1e8dd90120d4a2afaccaa4
Neraky <felipecartaxo1@gmail.com>
2025-01-25 19:50:28 -0300

    atualizando imports

--- src/app/professor/professor.module.ts
+++ src/app/professor/professor.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ListagemProfessorComponent } from './listagem-professor/listagem-professor.component';
-
-
+import { CadastroProfessorComponent } from './cadastro-professor/cadastro-professor.component';
+import { ListagemCadastroProfessorComponent } from './listagem-cadastro-professor/listagem-cadastro-professor.component';
 
 @NgModule({
   declarations: [
-    ListagemProfessorComponent
+    ListagemProfessorComponent,
+    CadastroProfessorComponent,
+    ListagemCadastroProfessorComponent
   ],
   imports: [
     CommonModule
