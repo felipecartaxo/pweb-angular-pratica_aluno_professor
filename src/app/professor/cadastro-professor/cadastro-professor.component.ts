ccd4bf4fb3b42d03ce254462a79bc73d938649ec
Neraky <felipecartaxo1@gmail.com>
2025-01-25 19:37:26 -0300

    criando componente de cadastro de professores

---
+++ src/app/professor/cadastro-professor/cadastro-professor.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cadastro-professor',
+  standalone: false,
+  
+  templateUrl: './cadastro-professor.component.html',
+  styleUrl: './cadastro-professor.component.css'
+})
+export class CadastroProfessorComponent {
+
+}
