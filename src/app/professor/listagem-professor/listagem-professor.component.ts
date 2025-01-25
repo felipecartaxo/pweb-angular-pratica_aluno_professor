af4f40525cb00488eb6fc3f582643b48db02791c
Neraky <felipecartaxo1@gmail.com>
2025-01-25 19:13:14 -0300

    criando módulos e componentes para Professor

---
+++ src/app/professor/listagem-professor/listagem-professor.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-listagem-professor',
+  standalone: false,
+  
+  templateUrl: './listagem-professor.component.html',
+  styleUrl: './listagem-professor.component.css'
+})
+export class ListagemProfessorComponent {
+
+}
---
+++ src/app/professor/professor.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ListagemProfessorComponent } from './listagem-professor/listagem-professor.component';
+
+
+
+@NgModule({
+  declarations: [
+    ListagemProfessorComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class ProfessorModule { }
