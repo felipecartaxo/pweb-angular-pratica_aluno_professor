67bd70749cd41c57e840faf402ec5036ea855aa8
Neraky <felipecartaxo1@gmail.com>
2025-01-25 19:05:53 -0300

    criando classe Professor

---
+++ src/app/shared/modelo/professor.ts
@@ -0,0 +1,8 @@
+export class Professor {
+
+    constructor(public matricula: string='',
+                public nome: string = '',
+                public idade: number = 0
+                ) {
+    }
+}
