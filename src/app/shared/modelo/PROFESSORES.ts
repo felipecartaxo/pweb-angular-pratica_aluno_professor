adeee2fed1ba0939a1e716836a37ab6b3bc4a409
Neraky <felipecartaxo1@gmail.com>
2025-01-25 19:08:48 -0300

    criando classe PROFESSORES

---
+++ src/app/shared/modelo/PROFESSORES.ts
@@ -0,0 +1,7 @@
+import { Professor } from "./professor";
+
+export const PROFESSORES: Professor[] = [
+    new Professor('12345', 'Carlos', 42),
+    new Professor('67890', 'Ana', 25),
+    new Professor('00000', 'Karen', 28)
+];
